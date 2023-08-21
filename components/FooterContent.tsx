ecef75eb6a5b774cc8cbdcb2b9e085ddd2726817
TochKa21U <tochka.mg@proton.me>
2023-08-21 22:57:53 +0200

    Small changes

--- components/FooterContent.tsx
+++ components/FooterContent.tsx
@@ -8,7 +8,7 @@ const FooterContent: React.FC = () => {
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="https://github.com/" className="flex items-center mb-4 sm:mb-0">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Injexicon Logo" />
+                        {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Injexicon Logo" /> */}
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Injexicon</span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 p-4 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
